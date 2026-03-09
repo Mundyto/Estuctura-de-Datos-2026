e57d5d5beff3e603d675ee4770937706fcb7c814
21844 <42402149@uaz.edu.mx>
2026-03-09 08:04:10 -0600

    correccion

--- Practica 3/bicolas.py
+++ Practica 3/bicolas.py
@@ -10,19 +10,19 @@ def enque_tail(lista, elemento):
 def deque_tail(lista):
     return lista.pop()
 
-def retiros(saldo, retiro):
-    monto_retiro = deque_head(retiro)
-    saldo_actual = deque_head(saldo)
+def retiros(saldo_list, retiro_queue):
+    monto_retiro = deque_head(retiro_queue)
+    saldo_actual = deque_head(saldo_list)
 
     nuevo_saldo = saldo_actual - monto_retiro
-    enque_tail(saldo, nuevo_saldo)
+    enque_tail(saldo_list, nuevo_saldo)
 
-def depositos(saldo, deposito):
-    monto_deposito = deque_tail(deposito)
-    saldo_actual = deque_head(saldo)
+def depositos(saldo_list, deposito_list):
+    monto_deposito = deque_tail(deposito_list)
+    saldo_actual = deque_head(saldo_list)
 
     nuevo_saldo = saldo_actual + monto_deposito
-    enque_tail(saldo, nuevo_saldo)
+    enque_tail(saldo_list, nuevo_saldo)
 
 saldo = [1000,1000,1000,1000,1000]
 retiro = [500,500,500,500,500]
